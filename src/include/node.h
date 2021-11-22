e5f8bb341fe3c6ba19708253b59bf2df5de225a1
Andrecik <cicchiniandrea@outlook.com>
2021-11-22 19:37:28 +0100

    const keytype

--- src/include/node.h
+++ src/include/node.h
@@ -3,13 +3,13 @@
 
 #include <iostream>
 #include <memory>
-
+#include <utility>
 
 
 template <typename kT, typename vT>
 class Node{
 private:    
-    std::pair<kT key, vT value>;
+    std::pair<kT, vT> element;//riflettere su se dobbiamo mantere costante il valore della key: aggiungere "const kT"
     std::unique_ptr<Node> r_next; //uniquepointer
     std::unique_ptr<Node> l_next; //uniquepointer
 
--- src/node.cc
+++ src/node.cc
@@ -1,22 +1,4 @@
 #include "include/node.h"
 
 
-template <typename T>
-template <typename O>
-Node<T>::Node(O x):  element{x}, r_next{nullptr}, l_next{nullptr} {} 
 
-template <typename T>
-template <typename O>
-Node<T>::Node(O x, Node* right){
-    element = x;
-    r_next = right;
-    l_next = nullptr; 
-}
-
-template <typename T>
-template <typename O>
-Node<T>::Node(O x, Node* right, Node* left){
-    element = x;
-    r_next = right;
-    l_next = left; 
-}
