e31ada95b48520b4f5e8df957a02fdbc6040d8bd
Andrecik <cicchiniandrea@outlook.com>
2021-12-24 22:44:04 +0100

    non lo so se il main va bene

--- src/main.cc
+++ src/main.cc
@@ -1,345 +1,280 @@
 #include <iostream>
 #include <memory>
 #include <utility>
-// #include "include/node.h"
-// #include "include/iterator.h"
-// #include "include/bst.h"
-#include "bst.cc"
-#include "include/ap_error.h"
 #include <cstdlib>
 #include <ctime>
+#include "bst.cc"
+#include "include/ap_error.h"
 
-void full_the_tree(Bst<int, char>& tree, size_t count){
-  unsigned seed = time(0);
-  srand(seed);
-  for (size_t i = 0; i < count; i++)
-  {
-    tree.insert(std::pair{rand() % 100 + 1,'a' + rand()%26});
-  }
-}
-//implementare un operatore custom
+
+void fill_the_tree(Bst<int, char>& tree, size_t count, unsigned seed){
+    srand(seed);
+    for (size_t i = 0; i < count; i++)
+    {
+        tree.insert(std::pair{rand() % 100 + 1,'a' + rand()%26});
+    }
+    }
+
 int main(){
-  try {
-  
-// //caso 1
-// //inizializzo un albero
-// Bst<int, char> tree;
-// //inserisco nodi (volendo con valori random)
-// full_the_tree(tree,20);
-// //printa l'albero
-// std::cout<< tree <<" \n";
-
-// // caso 2
-// // inizializzo un albero
-// Bst<int, char> tree1;
-// // inserisco nodi
-// full_the_tree(tree1,20);
-// // printa l'albero
-// std::cout<< tree1 <<" \n";
-// // copia l'albero 
-// Bst<int, char> tree2{tree1};
-// // modifica il secondo albero
-// full_the_tree(tree2, 1);
-// // printa entrambi gli alberi
-// std::cout<< tree1 <<" \n";
-// std::cout<< tree2 <<" \n";
-
-// // caso 3
-// // inizializzo un albero
-// Bst<int, char> tree1;
-// // inserisco nodi
-// full_the_tree(tree1, 1);
-// // printa l'albero
-// std::cout<< tree1 <<" \n";
-// // muovi su di un altro albero
-
-// // printa il secondo albero
-
-// // caso 4
-// // inizializzo un albero
-// // inserisco nodi con emplace
-// // printo l'albero
-
-// caso 5
-// inizializzo un albero
-//Bst<int, char> tree;
-// inserisco nodi
-//full_the_tree(tree, 1);
-// printo l'albero
-//std::cout<<tree<<" \n";
-// eseguo clear
-// printo l'albero
-// inserisco nodi 
-// printo l'albero
-
-// caso 6
-// inizializzo un albero
-// inserisco nodi
-// printo l'albero
-// cerco un nodo specifico
-// printo gli elementi del nodo
-
-// caso 7
-// inizializzo un albero
-// inserisco nodi in maniera sbilanciata
-// printo l'albero
-// bilancio l'albero
-// printo l'albero
-
-// caso 8
-// inizializzo un albero
-// inserisco nodi
-// printo l'albero
-// cancello un nodo 
-// printo l'albero
-
-// caso 10
-// inizializzo un albero
-// inserisco nodi
-// printo l'albero
-// accedo ad un nodo con []
-// modifico il nodo
-// printo l'albero
-// inserisco un nodo senza value
-// accedo al nodo con []
-// printo il value di quel nodo
-// printo l'albero
-
-
-
-
-
-
-    // Bst<int,char> tree;
-    // std::pair<int,char> p1{10,'b'};
-    // std::pair<int,char> p2{8,'c'};
-    // std::pair<int,char> p3{7,'d'};
-    // std::pair<int,char> p4{2,'f'};
-    // std::pair<int,char> p5{1,'z'};
-    // std::pair<int,char> p6{3,'y'};
-    // std::pair<int,char> p7{4,'b'};
-    // std::pair<int,char> p8{15,'t'};
-    // std::pair<int,char> p9{14,'r'};
-    // std::pair<int,char> p10{17,'s'};
-    // std::pair<int,char> p11{13,'l'};
-    // std::pair<int,char> p12{22,'m'};
-    // std::pair<int,char> p13{11,'n'};
-    // std::pair<int,char> p14{19,'u'};
-    // std::pair<int,char> p15{16,'x'};
-    // tree.insert(p1);
-    // tree.insert(p2);
-    // tree.insert(p3);
-    // tree.insert(p4);
-    // tree.insert(p5);
-    // tree.insert(p6);
-    // tree.insert(p7);
-    // tree.insert(p8);
-    // tree.insert(p9);
-    // tree.insert(p10);
-    // tree.insert(p11);
-    // tree.insert(p12);
-    // tree.insert(p13);
-    // tree.insert(p14);
-    // tree.insert(p15);
-    // std::cout<<tree<<std::endl;
-    // tree.erase(7);
-    // std::cout<<tree<<std::endl;
-    // auto a = tree.find(p12.first);
-    // std::cout<<"\n FIND? "<< p12.first<< " " << (*a).first<<" \n";
-    // a = tree.find(16);
-    // std::cout<<"\n FIND? "<< 16 << " " << (*a).first<<" \n";
-    // a = tree.find(7);
-    // std::cout<<"\n FIND? "<< 16 << " " << (*a).first<<" \n";
-
-
-    Bst<int,char> tree;
-    std::pair<int,char> p1{1,'b'};
-    std::pair<int,char> p2{2,'c'};
-    std::pair<int,char> p3{3,'d'};
-    std::pair<int,char> p4{4,'f'};
-    std::pair<int,char> p5{5,'z'};
-    std::pair<int,char> p6{6,'y'};
-    std::pair<int,char> p7{7,'b'};
-    // std::pair<int,char> p8{15,'t'};
-    // std::pair<int,char> p9{14,'r'};
-    // std::pair<int,char> p10{17,'s'};
-    // std::pair<int,char> p11{13,'l'};
-    // std::pair<int,char> p12{22,'m'};
-    // std::pair<int,char> p13{11,'n'};
-    // std::pair<int,char> p14{19,'u'};
-    // std::pair<int,char> p15{16,'x'};
-    tree.insert(p1);
-    tree.insert(p2);
-    tree.insert(p3);
-    tree.insert(p4);
-    tree.insert(p5);
-    tree.insert(p6);
-    tree.insert(p7);
-    // tree.insert(p8);
-    // tree.insert(p9);
-    // tree.insert(p10);
-    // tree.insert(p11);
-    // tree.insert(p12);
-    // tree.insert(p13);
-    // tree.insert(p14);
-    // tree.insert(p15);
-    tree.balance();
-    std::cout<<tree<<std::endl;
-    tree.erase(15);
-    std::cout<<tree<<std::endl;
-    auto a = tree.find(4);
-    //std::cout<<"\n FIND? "<< 4 << " " << (*a).first<<" \n";
-    a = tree.find(7);
-    //std::cout<<"\n FIND? "<< 7 << " " << (*a).first<<" \n";
-    a = tree.find(1);
-    //std::cout<<"\n FIND? "<< 2 << " " << (*a).first<<" \n";
-
-
-
-  //return 0;
-  //   Bst<int,char,std::less<int>> tree1{std::less<int>()};
-  //   std::cout << tree1 << std::endl; 
-  //   //Bst<int,char,std::less<int>> tree2{tree1};
-  //   std::cout<<"\n \nInserisco il primo nodo \n";
-  //   auto i = tree1.insert(std::make_pair(3,'a'));
-
-  //   std::cout << " \n provo vedere ";
-  //   std::cout<<(*(i.first)).first<<std::endl;
-  //   std::cout<<"\n \nInserisco il secondo nodo \n";
-  //   i = tree1.insert(std::make_pair(3,'b'));
-
-  //   std::cout << " \n provo vedere ";
-  //   std::cout<<(*(i.first)).first<<std::endl;
-  //   if (!i.second){
-  //     std::cout<< "Already in the tree \n";
-
-  //   }
-  //   else{std::cout<< "insert \n";}
-  //   std::cout<<"\n \nInserisco il terzo nodo \n";
-  //   i = tree1.insert(std::make_pair(4,'b'));
-
-  //   std::cout << " \n provo vedere ";
-  //   std::cout<<(*(i.first)).first<<std::endl;
-  //   if (!i.second){
-  //     std::cout<< "Already in the tree \n";
-
-  //   }
-  //   else{std::cout<< "insert \n";}
-  //       i = tree1.insert(std::make_pair(2,'c'));
-
-  //   std::cout << " \n provo vedere ";
-  //   std::cout<<(*(i.first)).first<<std::endl;
-  //   if (!i.second){
-  //     std::cout<< "Already in the tree \n";
-
-  //   }
-  //   else{std::cout<< "insert \n";}
-  //   std::cout<<"\n \nInserisco il quarto nodo \n";
-  //       i = tree1.insert(std::make_pair(5,'d'));
-
-  //   std::cout << " \n provo vedere ";
-  //   std::cout<<(*(i.first)).first<<std::endl;
-  //   if (!i.second){
-  //     std::cout<< "Already in the tree \n";
-
-  //   }
-  //   else{std::cout<< "insert \n";}
+    try {
+    int choice;
+    bool get_out= true;
+    unsigned seed = time(0);
+do
+{
     
-  //   tree1.tryfor();
+    std::cout<<"********************************************\n"; 
+    std::cout<<"1 - Test 1 \n";
+    std::cout<<"2 - Test 2 \n";
+    std::cout<<"3 - Test 3 \n";
+    std::cout<<"4 - Test 4 \n";
+    std::cout<<"5 - Test 5 \n";
+    std::cout<<"6 - Test 6 \n";
+    std::cout<<"7 - Test 7 \n";
+    std::cout<<"8 - Test 8 \n";
+    std::cout<<"9 - Test 9 \n";
+    std::cout<<"10 - print tests decriptions\n";
+    std::cout<<"11 - Exit \n";
+    std::cout<<"Enter your  choce and press return :  ";      
 
-  //   // const Bst<int,char,std::less<int>> tree2{std::less<int>()};
+    std::cin >> choice;
 
-  //   //std::cout << *(tree2.end())
+    if (choice == 11)
+    {
+        exit(0);
+    }
+    else if(choice == 1) 
+    {
+        std::cout<<"Test 1 \nwe create a tree and we print it \n";
+        //inizializzo un albero
+        Bst<int, char> tree;
+        //inserisco nodi (volendo con valori random)
+        fill_the_tree(tree,20,seed);
+        //printa l'albero
+        std::cout<< tree <<" \n";
 
-  //   std::cout<< "provo a printare \n";
-  //   std::cout << tree1 << std::endl;
-  //   i = tree1.emplace(8,'g');
+    }
+    else if(choice == 2) 
+    {   std::cout<<"Test 2 \nwe create a tree and we print it \n";
+        // inizializzo un albero
+        Bst<int, char> tree1;
+        // inserisco nodi
+        fill_the_tree(tree1,20,seed);
+        // printa l'albero
+        std::cout<< tree1 <<" \n";
+        std::cout<<"Then we copy tree1 into tree 2 \nwe insert a new node into tree2 and then we print both";
+        // copia l'albero 
+        Bst<int, char> tree2{tree1};
+        // modifica il secondo albero
+        fill_the_tree(tree2, 1, 42);
+        // printa entrambi gli alberi
+        std::cout<< tree1 <<" \n";
+        std::cout<< tree2 <<" \n";
 
-  //   std::cout << " \n provo vedere ";
-  //   std::cout<< " 8 " << std::endl;
-  //   std::cout<<(*(i.first)).first<<std::endl;
-  //   std::pair<const int, char> p2{1,'e'};
-  //   std::pair<int, char> p3{7,'f'}; 
-  //   i = tree1.insert(p2);
-  //   std::cout << " \n provo vedere ";
-  //   std::cout<<p2.first << " " << std::endl;
-  //   std::cout<<(*(i.first)).first<<std::endl;
+    }
+    else if(choice == 3) 
+    {   std::cout<<"Test 3 \nwe create a tree and we print it \n";
+        // caso 3
+        // inizializzo un albero
+        Bst<int, char> tree1;
+        // inserisco nodi
+        fill_the_tree(tree1, 20,seed);
+        // printa l'albero
+        std::cout<< tree1 <<" \n";
+        std::cout<<"Then we move tree1 into tree2 \nwe insert a new node into tree2 and then we print both";
+        // muovi su di un altro albero
+        Bst<int, char> tree2 = std::move(tree1);
+        // printa il secondo albero
+        std::cout<< tree2 <<" \n";
 
+    }
+    else if(choice == 4) 
+    {
+        //caso 4
+        // inizializzo un albero
+        std::cout<<"Test 4 \nwe create a tree and we print it \n";
+        Bst<int, char> tree;
+        fill_the_tree(tree, 20,seed);
+        std::cout<< tree <<" \n";
+        // inserisco nodi con emplace
+        std::cout<<"we use emplace and we print the tree\n";
+        tree.emplace(42,'b');
+        // printo l'albero
+        std::cout<< tree <<" \n";
+    }
+    else if(choice == 5) 
+    {
+        // caso 5
+        // inizializzo un albero
+        Bst<int, char> tree;
+        // inserisco nodi
+        fill_the_tree(tree, 20,seed);
+        // printo l'albero
+        std::cout<<tree<<" \n";
+        // eseguo clear
+        tree.clear();
+        // printo l'albero
+        std::cout<<tree<< " \n";
+        // inserisco nodi 
+        fill_the_tree(tree, 20, 56);
+        // printo l'albero
+        std::cout<<tree<< " \n";
 
+    }
+        else if(choice == 6) 
+    {   // inizializzo un albero
+        Bst<int, char> tree;
+        // inserisco nodi
+        fill_the_tree(tree, 20,seed);
+        // printo l'albero
+        std::cout<<tree<<" \n";
+        tree.emplace(42,'b');
+        // cerco un nodo che c'è
+        tree.find(42);
+        tree[42];
+        //cerco un nodo a caso
+        tree.find(82);
+        tree[82];
+    }
+        else if(choice == 7) 
+    {
+        Bst<int, char> tree;
+        // inserisco nodi
+        fill_the_tree(tree, 20,seed);
+        // inserisco nodi in maniera sbilanciata
+        // printo l'albero
+        std::cout<<tree<<" \n";
+        tree.balance();
+        // bilancio l'albero
+        std::cout<<tree<<" \n";
+        // printo l'albero
+    }
+        else if(choice == 8) 
+    {   
+        // inizializzo un albero
+        Bst<int, char> tree;
+        // inserisco nodi
+        fill_the_tree(tree, 10,seed);
+        // inserisco nodi in maniera sbilanciata
+        // printo l'albero
+        tree.emplace(42,'c');
+        fill_the_tree(tree, 10, 42);
+        std::cout<<tree<<" \n";
+        // inserisco nodi
+        // printo l'albero
+        // cancello un nodo
+        tree.erase(42);
+        // printo l'albero
+        std::cout<<tree<<" \n";
+    }
+        else if(choice == 9) 
+    {
+        Bst<int, char> tree;
+        // inserisco nodi
+        fill_the_tree(tree, 20, 42);
+        std::cout<<tree<<" \n";
+        // accedo ad un nodo con []
+        auto a = tree.emplace(42,'c');
+        std::cout<< tree[42] << " \n";
+        // modifico il nodo
+        tree[42]='f';
+        std::cout<< tree[42] << " \n";
+        // printo l'albero
+        std::cout<<tree<<" \n";
+        // inserisco un nodo senza value
+        std::cout<< tree[180] << " \n";
+        // accedo al nodo con []
+        a = tree.emplace(182,' ');
+        std::cout<< tree[182] << " \n";
+        // printo il value di quel nodo
+        // printo l'albero
 
-  //   i = tree1.insert(p3);
-  //   std::cout << " \n provo vedere ";
-  //   std::cout<<p3.first << " " << std::endl;
-  //   std::cout<<(*(i.first)).first<<std::endl;
-    
-  //       std::cout<< "provo a printare 2\n";
-  //       std::cout<<p2.first << " " << std::endl;
-  //   std::cout << tree1 << std::endl;
+    }
+        else if(choice == 10) 
+    {   
+        std::cout<<"Test 1 \n"
+        <<"initialize a tree \n"
+        <<"insert nodes \n"
+        <<"print the tree \n"
+        <<"\n \n"
+        <<"Test 2 \n"
+        <<"initialize a tree \n"
+        <<"insert nodes \n"
+        <<"print the tree \n"
+        <<"copy the tree \n"
+        <<"modify the second tree \n"
+        <<"print both trees \n"
+        <<"\n \n"
+        <<"Test 3 \n"
+        <<"initialize a tree \n"
+        <<"insert nodes \n"
+        <<"print the tree \n"
+        <<"move the tre into a second tree \n"
+        <<"print the tree \n"
+        <<"\n \n"
+        <<"Test 4 \n"
+        <<"initialize a tree \n"
+        <<"use emplace \n"
+        <<"print the tree \n"
+        <<"\n \n"
+        <<"Test 5 \n"
+        <<"initialize a tree \n"
+        <<"insert nodes \n"
+        <<"print the tree \n"
+        <<"use clear"
+        <<"initialize a tree \n"
+        <<"insert nodes \n"
+        <<"print the tree \n"
+        <<"\n \n"
+        <<"Test 6 \n"
+        <<"initialize a tree \n"
+        <<"insert nodes \n"
+        <<"print the tree \n"
+        <<"find a specific node \n"
+        <<"print the node element \n"
+        <<"\n \n"
+        <<"Test 7 \n"
+        <<"initialize a tree \n"
+        <<"insert nodes \n"
+        <<"print the tree \n"
+        <<"balance the tree \n"
+        <<"print the tree \n"
+        <<"\n \n"
+        <<"Test 8 \n"
+        <<"initialize a tree \n"
+        <<"insert nodes \n"
+        <<"print the tree \n"
+        <<"erase the node \n"
+        <<"print the tree \n"
+        <<"\n \n"
+        <<"Test 9 \n"
+        <<"initialize a tree \n"
+        <<"insert nodes \n"
+        <<"print the tree \n"
+        <<"access a node with square bracket \n"
+        <<"modify the node \n"
+        <<"print the tree \n"
+        <<"print the tree \n";
+        }
 
-  //   std::cout << " \n provo a bilanciare \n \n ";
-    
-  //   tree1.balance();
-  //       std::cout<< "provo a printare 3 \n";
-  //   std::cout << tree1 << std::endl;
+    else
+    {
+        std::cout<<"Sorry choice not implemented yet\n" ;
+        get_out=false;
 
+    }
 
-  //   Bst<int,char,std::less<int>> tree2{tree1};
-  //   std::cout<< "provo a printare il secondo albero \n";
-  //   std::cout << tree2 << std::endl;
-
-
-  //   auto a = tree1.find(p3.first);
-  //   std::cout<<"\n FIND? "<< (*a).first<<" \n";
-  //   std::cout<< "provo a printare il secondo albero \n";
-  //   std::cout << tree2 << std::endl;
-  //   std::cout<< "TESTO LE PARENTESI \n";
-  //   auto k = tree2[3];
-  //   std::cout<< "\n PARENTESI QUEDRE \n";
-  //   std::cout << k << std::endl;
-
-
-  //   k = tree2[17];
-  //   std::cout<< "\n PARENTESI QUEDRE \n";
-  //   std::cout << k << std::endl;
-
-  // std::cout<< "TESTO LE PARENTESI 2 \n";
-  //   int bun=7;
-  //   k = tree2[bun];
-  //   std::cout<< "\n PARENTESI QUEDRE \n";
-  //   std::cout << k << std::endl;
-
-  //   bun= 17;
-
-  //   k = tree2[bun];
-  //   std::cout<< "\n PARENTESI QUEDRE \n";
-  //   std::cout << k << std::endl;
-
-
-
-  //   std::cout<< "provo a printare il primo albero \n";
-  //   std::cout << tree1 << std::endl;
-  //   tree1.erase(p3.first);
-  //   std::cout<< "provo a printare il primo albero \n";
-  //   std::cout << tree1 << std::endl;
-
-
-  //   std::cout<< "provo a printare il secondo albero \n";
-  //   std::cout << tree2 << std::endl;
-
-  //   tree1.clear();
-  //   std::cout<< "provo a printare il secondo albero \n";
-  //   std::cout << tree2 << std::endl;
-
+} while (get_out == true);
 
     return 0;
 
-  } catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  } catch (...) {
-    std::cerr << "Unknown exception" << std::endl;
-  }
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Unknown exception" << std::endl;
+    }
 
-  
-  
-    return 0;
+    
+    
+        return 0;
 }
